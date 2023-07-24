13f250356b1460716e9e783a3f50c3ee88942d0e
boro-dach <monstriqq1@gmail.com>
2023-07-24 22:30:10 +0200

    Calculator-BMI
    added BMI counting and weight recomendations

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 document.querySelector('#sum').addEventListener('click', getInfo)
 
 function getInfo(){
-    let height = document.getElementById('height').value
-    console.log(height)
-    let weight = document.getElementById('weight').value
-    console.log(weight)
+    let height = document.getElementById('height').value;
+    let weight = document.getElementById('weight').value;
+    let heightRes = Math.pow(height / 100, 2);
+    let result = weight / heightRes;
+    let paragraph = document.querySelector('.result');
+    paragraph.textContent = `Your BMI is: ${result.toFixed(2)}`;
+    let recText = document.querySelector('.rec');
+    if(result <= 19){
+        recText.textContent = 'Your weight is too low. Eat more!';
+    }
+    else if(result > 19 & result <= 26){
+        recText.textContent = 'Your weight is perfect. You are cool!';
+    }
+    else if(result > 26){
+        recText.textContent = 'Your weight is too big. Eat less!';
+    }
 }
