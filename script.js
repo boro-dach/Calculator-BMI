97aba5566e31b4b66ec95e57df1dff0e310f8cf3
boro-dach <monstriqq1@gmail.com>
2023-07-24 21:06:23 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('#sum').addEventListener('click', getInfo)
+
+function getInfo(){
+    let height = document.getElementById('height').value
+    console.log(height)
+    let weight = document.getElementById('weight').value
+    console.log(weight)
+}
